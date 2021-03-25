d1c8665c9d70f3335723875cec4529d64ae41cee
Iran Filho <Iran_filho@yahoo.com.br>
2021-03-25 10:50:52 -0300

    25/03/21 - resolução de exercício

---
+++ contador-03.py
@@ -0,0 +1,12 @@
+def main():
+    a=int(12)
+    b=int(17)
+    c=int(4)
+    d=int(-6)
+    e=int(8)
+    f=int(0)
+
+    soma=a+b+c+d+e+f
+
+    print("o valor final do resultado do código é", soma)
+main()

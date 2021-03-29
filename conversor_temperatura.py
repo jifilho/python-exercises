81e4cc1034cad39c6996967169de5ccd4ef6a46c
Iran Filho <Iran_filho@yahoo.com.br>
2021-03-29 10:39:09 -0300

    novos exercícios
    29/03/21

---
+++ conversor_temperatura.py
@@ -0,0 +1,7 @@
+temperaturaFarenheit=input("Digite uma temperatura em Farenheit:")
+
+temp=float(temperaturaFarenheit)
+
+temperaturaCelsius=(temp-32)*5/9
+
+print("A temperatura em celsius é", temperaturaCelsius)

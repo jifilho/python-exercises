34618a7e558e1ad0fe0b7084e293f5fcaef11b60
Iran Filho <Iran_filho@yahoo.com.br>
2021-03-31 10:27:11 -0300

    Calculador de média aritimética
    Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética

---
+++ lista/ex-02.py
@@ -0,0 +1,9 @@
+primeira=float(input("Digite sua primeira nota: "))
+segunda=float(input("Digite sua segunda nota: "))
+terceira=float(input("Digite sua terceira nota: "))
+quarta=float(input("Digite sua quarta nota: "))
+
+result=primeira+segunda+terceira+quarta
+result_final=result/4
+
+print("Sua média aritimética é", result_final)
